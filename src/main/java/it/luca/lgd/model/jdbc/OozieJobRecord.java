7265d8a2ad94ed9cb19ee89c0f2d8bdd9bd7f0f4
carloxluca91 <carloni.luca91@gmail.com>
2021-03-01 00:32:47 +0100

    enriched TimeUtils

--- src/main/java/it/luca/lgd/model/jdbc/OozieJobRecord.java
+++ src/main/java/it/luca/lgd/model/jdbc/OozieJobRecord.java
@@ -1,6 +1,7 @@
 package it.luca.lgd.model.jdbc;
 
 import it.luca.lgd.oozie.OozieJobType;
+import it.luca.lgd.utils.TimeUtils;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.oozie.client.WorkflowAction;
@@ -11,11 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Data
 @Entity
@@ -40,14 +38,6 @@ public class OozieJobRecord {
 
     public static OozieJobRecord fromWorkflowJob(WorkflowJob workflowJob) {
 
-        Function<java.util.Date, LocalDate> toLocalDate = date -> Optional.ofNullable(date)
-                .map(d -> d.toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
-                .orElse(null);
-
-        Function<java.util.Date, LocalDateTime> toLocalDateTime = date -> Optional.ofNullable(date)
-                .map(d -> LocalDateTime.ofInstant(d.toInstant(), ZoneId.systemDefault()))
-                .orElse(null);
-
         List<WorkflowAction.Status> completedStatuses = Arrays.asList(WorkflowAction.Status.DONE,
                 WorkflowAction.Status.FAILED,
                 WorkflowAction.Status.KILLED,
@@ -59,10 +49,10 @@ public class OozieJobRecord {
         oozieJobRecord.setJobName(workflowJob.getAppName());
         oozieJobRecord.setJobUser(workflowJob.getUser());
         oozieJobRecord.setJobStatus(workflowJob.getStatus().toString());
-        oozieJobRecord.setJobStartDate(toLocalDate.apply(workflowJob.getStartTime()));
-        oozieJobRecord.setJobStartTime(toLocalDateTime.apply(workflowJob.getStartTime()));
-        oozieJobRecord.setJobEndDate(toLocalDate.apply(workflowJob.getEndTime()));
-        oozieJobRecord.setJobEndTime(toLocalDateTime.apply(workflowJob.getEndTime()));
+        oozieJobRecord.setJobStartDate(TimeUtils.fromUtilDateToLocalDate(workflowJob.getStartTime()));
+        oozieJobRecord.setJobStartTime(TimeUtils.fromUtilDateToLocalDateTime(workflowJob.getStartTime()));
+        oozieJobRecord.setJobEndDate(TimeUtils.fromUtilDateToLocalDate(workflowJob.getEndTime()));
+        oozieJobRecord.setJobEndTime(TimeUtils.fromUtilDateToLocalDateTime(workflowJob.getEndTime()));
         oozieJobRecord.setJobTotalActions(workflowJob.getActions().size());
         oozieJobRecord.setJobCompletedActions((int) workflowJob.getActions().stream()
                 .filter(workflowAction -> completedStatuses.contains(workflowAction.getStatus()))
--- src/main/java/it/luca/lgd/utils/TimeUtils.java
+++ src/main/java/it/luca/lgd/utils/TimeUtils.java
@@ -1,14 +1,35 @@
 package it.luca.lgd.utils;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class TimeUtils {
 
+    public static String changeDateFormat(String date, String oldFormat, String newFormat) {
+
+        return toString(toLocalDate(date, oldFormat), newFormat);
+    }
+
+    public static LocalDate fromUtilDateToLocalDate(java.util.Date date) {
+
+        return Optional.ofNullable(date)
+                .map(d -> d.toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .orElse(null);
+    }
+
+    public static LocalDateTime fromUtilDateToLocalDateTime(java.util.Date date) {
+
+        return Optional.ofNullable(date)
+                .map(d -> LocalDateTime.ofInstant(d.toInstant(), ZoneId.systemDefault()))
+                .orElse(null);
+    }
+
     public static Boolean isValidDate(String date, String format) {
 
-        try {
-            toLocalDate(date, format);
+        try { toLocalDate(date, format);
         } catch (Exception e) {
             return false;
         }
@@ -20,11 +41,6 @@ public class TimeUtils {
 
         return toLocalDate(firstDate, commonFormat)
                 .compareTo(toLocalDate(seconDate, commonFormat)) <= 0;
-    }
-
-    public static String changeDateFormat(String date, String oldFormat, String newFormat) {
-
-        return toString(toLocalDate(date, oldFormat), newFormat);
     }
 
     public static LocalDate toLocalDate(String date, String format) {
